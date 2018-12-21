af57abb0254cc32b33bd723fb3a54470ca220a8f
Manuel <majesova@gmail.com>
2018-12-21 11:43:55 -0600

    se agrega random 0,1

---
+++ Samples/tests.py
@@ -0,0 +1,19 @@
+# Módulos importados
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import math
+
+inputs = np.array([[0,1], [0,0], [1,1], [1,0], [0,1] ,[1,1]])
+
+values = np.zeros(1 + inputs.shape[1])
+
+values2 = np.random.choice([0., 1.], size=(3,), p=[.5, .5])
+
+np.random.choice([0., 1.], size=((1 + inputs.shape[1]),), p=[.5, .5])
+
+
+print(values)
+print(values2)
+
